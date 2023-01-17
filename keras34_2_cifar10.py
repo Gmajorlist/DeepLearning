c3e59b0b537029138b2c538ff9926338d83af5d6
Gmajorlist <121652054+Gmajorlist@users.noreply.github.com>
2023-01-17 18:21:52 +0900

    Add files via upload

--- keras34_2_cifar10.py
+++ keras34_2_cifar10.py
@@ -4,7 +4,7 @@ from tensorflow.keras.datasets import mnist
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint 
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D ,Dense, Flatten,MaxPooling2D, Dropout
+from tensorflow.keras.layers import Conv2D ,Dense, Flatten,MaxPooling2D, Dropout ,BatchNormalization
 
 
 path = 'C:/study/keras_save/MCP/'
@@ -19,23 +19,26 @@ print(np.unique(y_train, return_counts=True))
 x_test, x_valid, y_test, y_valid = train_test_split(x_test, y_test,
                               train_size=0.8, random_state=123)
 
+# 
 model = Sequential()
-model.add(Conv2D(filters=128, kernel_size=(2,2), input_shape=(32,32,3),  
-                 activation='relu'))                                          
-model.add(Conv2D(filters=64, kernel_size=(2,2))) 
+model.add(Conv2D(filters=32, kernel_size=(3,3), kernel_initializer='he_uniform', input_shape=(32,32,3),  
+                 activation='relu')) 
+model.add(BatchNormalization())                                         
+model.add(Conv2D(filters=32, kernel_size=(3,3), kernel_initializer='he_uniform',activation='relu')) 
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2))) 
 model.add(Dropout(0.25))                      
-model.add(Conv2D(filters=64, kernel_size=(2,2)))
+model.add(Conv2D(filters=64, kernel_size=(3,3),kernel_initializer='he_uniform',activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=64, kernel_size=(3,3),kernel_initializer='he_uniform',activation='relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2))) 
-model.add(Dropout(0.25))                              
-model.add(Dense(64, activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2))) 
-model.add(Dropout(0.25))            
-# model.add(Dense(units=32, activation='relu'))
-# model.add(MaxPooling2D(pool_size=(2,2))) 
-# model.add(Dropout(0.25))   
-model.add(Flatten())                      
+model.add(Dropout(0.25))   
+model.add(Flatten())                           
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))                   
 model.add(Dense(10, activation= 'softmax'))
+
 
 
 #컴파일 훈련
@@ -66,4 +69,4 @@ print('loss : ', results[0] )
 print('acc:', results[1])
 
 #loss :  1.0717706680297852
-# acc: 0.6259999871253967
+# acc: 0.625999987125396
