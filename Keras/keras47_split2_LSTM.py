7e473c13d12d84f1c305fc0673e0e1e5fc5a5f33
darkmoster@naver.com <darkmoster@naver.com>
2023-01-27 11:34:19 +0900

    0127

--- Keras/keras47_split2_LSTM.py
+++ Keras/keras47_split2_LSTM.py
@@ -2,46 +2,53 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM
 
-a = np.array(range(1,101))
-
-timesteps = 5  # x 는 4개       y 는 1개
+dataset = np.array(range(1, 101)) 
+pre_data = np.array(range(96, 106))  
+timesteps = 5
 
 def split_x(dataset, timesteps):
-    aaa = []
-    for i in range(len(dataset) - timesteps + 1 ):
-        subset = dataset[i : (i + timesteps)] 
-        aaa.append(subset)
-    return np.array(aaa)
+    tmp = []
+    for i in range(len(dataset) - timesteps + 1):
+        subset = dataset[i : (i + timesteps)]
+        tmp.append(subset)
+    return np.array(tmp)
 
-bbb = split_x(a, timesteps)
+dataset = split_x(dataset, timesteps) 
+pre_data = split_x(pre_data, timesteps)
 
-print(bbb)
-print(bbb.shape)
+print("dataset.shape :", dataset.shape)
+print("pre_data.shape: ", pre_data.shape)
 
-x = bbb[:, :-1]
-y = bbb[:, -1]
-print(x, y)
-print(x.shape , y.shape) # (96, 4) (96,)
+x = dataset[:, :-1] 
+y = dataset[:, -1]
 
-x_predict = np.array(range(96, 106)) # 예상 y = 100, 107  
+pre_data = pre_data[:, :-1] 
+print("pre_data : ", pre_data)
+print("pre_data.shape : ", pre_data.shape)
 
-model = Sequential()              
-model.add(LSTM(units = 64, input_shape=(4, 1), activation='relu' ,
-               return_sequences=True))  
+print("x.shape: ", x.shape, "y.shape: ", y.shape)
+
+x = x.reshape(-1, 4, 1)
+
+model = Sequential()                                                                      
+model.add(LSTM(units = 64, input_shape=(4, 1), return_sequences=True, activation='relu')) 
+model.add(LSTM(64, activation = 'relu'))
 model.add(Dense(64, activation = 'relu'))
-model.add(Dense(32))
+model.add(Dense(64))
+model.add(Dense(64))
+model.add(Dense(16))
 model.add(Dense(1))
 model.summary() 
 
+
 model.compile(loss='mse', optimizer= 'adam')
-model.fit(x, y, epochs=200)
+model.fit(x, y, epochs=5)
 
 loss = model.evaluate(x, y)
 print(loss)
 
-y_predict = np.array(range(96, 106))
+y_predict = np.array(pre_data)
 
 result = model.predict(y_predict)
 
-print('prediction result: ', result) 
-
+print('prediction result:\n', result) 
